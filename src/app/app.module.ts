d8d704594916a62a26201c0e5167b01116cb9966
aromero <aromero@cikume.com>
2024-11-02 19:20:48 -0600

    Agregando el modulo y los primero componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CounterComponent } from './counter/counter.component';
 import { HeroComponent } from './heroes/hero/hero.component';
-import { ListComponent } from './heroes/list/list.component';
+import { DbzModule } from './dbz/dbz.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     CounterComponent,
     HeroComponent,
-    ListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    DbzModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
