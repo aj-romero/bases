9502b59333d53f36050750233fe52d96f4b5c8f3
Alber Romero <aromero@cikume.com>
2024-11-04 06:59:07 -0600

    ultima actualizacion de bases

--- src/app/heroes/list/list.component.ts
+++ src/app/heroes/list/list.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrl: './list.component.css'
 })
 export class ListComponent {
-
+ public heroNames: string[] = [
+  'Spiderman','Ironman','Hulk','Thor'
+ ];
 }
